94b8640594a803e3e0c1b2e811670a8b8d7a78c1
lucky <luckymourya52132@gmail.com>
2024-02-26 15:49:27 +0530

    prod39

--- src/main/java/com/learningplatform/app/smart_learn/config/MongoConfig.java
+++ src/main/java/com/learningplatform/app/smart_learn/config/MongoConfig.java
@@ -1,27 +1,44 @@
 package com.learningplatform.app.smart_learn.config;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-
 @Configuration
-@EnableMongoRepositories("com.learningplatform.app.smart_learn.repos")
-public class MongoConfig {
+@EnableMongoAuditing // If you need auditing capabilities
+public class MongoConfig extends AbstractMongoClientConfiguration {
+    // todo changes done in this classs
+    @Override
+    protected String getDatabaseName() {
+        return "smartlearndb2";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create("mongodb://mongo:e3a1e1eE-C2-2d4bE-hB4h-d226EcaGb@viaduct.proxy.rlwy.net:20713");
+    }
 
     @Bean
-    public MongoTransactionManager transactionManager(final MongoDatabaseFactory databaseFactory) {
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory databaseFactory) {
         return new MongoTransactionManager(databaseFactory);
     }
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener(
-            final LocalValidatorFactoryBean factory) {
+            LocalValidatorFactoryBean factory) {
         return new ValidatingMongoEventListener(factory);
     }
 
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory) {
+        return new MongoTemplate(mongoDbFactory);
+    }
 }
